6d1d914ecb28a0b598a92134fe1afa1ced3326b2
NTeditor <nteditor4@gmail.com>
2025-07-10 16:30:58 +0300

    SetupIptables разбит на функции; setupIptablesMultiport исправленно добавление mark и connbytes правил в setupIptablesMultiport при обработке udp

--- internal/iptables/iptables.go
+++ internal/iptables/iptables.go
@@ -49,9 +49,35 @@ func (ip *iptables) setupIptablesMultiport() {
 
 	log.Printf("Настройка iptables с multiport для UDP: %s", udpPortsString)
 	cmdArgsUDP := []string{"-t", "mangle", "-I", "POSTROUTING", "-p", "udp", "-m", "multiport", "--dports", udpPortsString, "-j", "NFQUEUE", "--queue-num", "200", "--queue-bypass"}
+	ip.enableMarkAndConnbytes(&cmdArgsUDP)
 
 	if err := exec.Command("iptables", cmdArgsUDP...).Run(); err != nil {
 		log.Fatalf("Ошибка настройки iptables (multiport UDP): %v", err)
+	}
+}
+
+func (ip *iptables) setupIptablesTCP() {
+	log.Println("Настройка iptables по отдельным портам.")
+	for _, port := range ip.nfqwsPortsTCP {
+		log.Printf("Добавление правила для TCP порта %s", port)
+		cmdArgs := []string{"-t", "mangle", "-I", "POSTROUTING", "-p", "tcp", "--dport", port, "-j", "NFQUEUE", "--queue-num", "200", "--queue-bypass"}
+		ip.enableMarkAndConnbytes(&cmdArgs)
+
+		if err := exec.Command("iptables", cmdArgs...).Run(); err != nil {
+			log.Printf("Ошибка добавления правила iptables для TCP порта %s; %s", port, err)
+		}
+	}
+}
+
+func (ip *iptables) setupIptablesUDP() {
+	for _, port := range ip.nfqwsPortsUDP {
+		log.Printf("Добавление правила для UDP порта %s", port)
+		cmdArgs := []string{"-t", "mangle", "-I", "POSTROUTING", "-p", "udp", "--dport", port, "-j", "NFQUEUE", "--queue-num", "200", "--queue-bypass"}
+		ip.enableMarkAndConnbytes(&cmdArgs)
+
+		if err := exec.Command("iptables", cmdArgs...).Run(); err != nil {
+			log.Printf("Ошибка добавления правила iptables для UDP порта %s: %v", port, err)
+		}
 	}
 }
 
@@ -59,31 +85,13 @@ func (ip *iptables) SetupIptables() {
 	if ip.multiportSupport {
 		ip.setupIptablesMultiport()
 	} else {
-		log.Println("Настройка iptables по отдельным портам.")
-		for _, port := range ip.nfqwsPortsTCP {
-			log.Printf("Добавление правила для TCP порта %s", port)
-			cmdArgs := []string{"-t", "mangle", "-I", "POSTROUTING", "-p", "tcp", "--dport", port, "-j", "NFQUEUE", "--queue-num", "200", "--queue-bypass"}
-			ip.enableMarkAndConnbytes(&cmdArgs)
-
-			if err := exec.Command("iptables", cmdArgs...).Run(); err != nil {
-				log.Printf("Ошибка добавления правила iptables для TCP порта %s; %s", port, err)
-			}
-		}
-
-		for _, port := range ip.nfqwsPortsUDP {
-			log.Printf("Добавление правила для UDP порта %s", port)
-			cmdArgs := []string{"-t", "mangle", "-I", "POSTROUTING", "-p", "udp", "--dport", port, "-j", "NFQUEUE", "--queue-num", "200", "--queue-bypass"}
-			ip.enableMarkAndConnbytes(&cmdArgs)
-
-			if err := exec.Command("iptables", cmdArgs...).Run(); err != nil {
-				log.Printf("Ошибка добавления правила iptables для UDP порта %s: %v", port, err)
-			}
-		}
+		ip.setupIptablesTCP()
+		ip.setupIptablesUDP()
 	}
 
 	log.Println("Установка sysctl net.netfilter.nf_conntrack_tcp_be_liberal=1")
 	if err := exec.Command("sysctl", "-w", "net.netfilter.nf_conntrack_tcp_be_liberal=1").Run(); err != nil {
-		log.Printf("Предупреждение: Не удалось установить sysctl: %v", err)
+		log.Printf("Предупреждение: Не удалось установить sysctl: %s", err)
 	}
 
 	log.Println("Настройка правил iptables завершена.")
